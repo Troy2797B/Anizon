3eb79b672be68cafb91d40b0b1b5e211dd088612
T B <tb@Ts-MacBook-Air.local>
2024-07-27 12:35:10 -0400

    adding the front end and back end to repository

---
+++ anizone_BE/src/main/java/passion/troy/anizone/AnizoneApplication.java
@@ -0,0 +1,12 @@
+package passion.troy.anizone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnizoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnizoneApplication.class, args);
+	}
+}
---
+++ anizone_BE/src/test/java/passion/troy/anizone/AnizoneApplicationTests.java
@@ -0,0 +1,13 @@
+package passion.troy.anizone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnizoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
