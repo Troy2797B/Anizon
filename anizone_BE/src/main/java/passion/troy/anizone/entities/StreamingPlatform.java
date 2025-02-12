a7f07a09eba189388e61c609606fed16023eb3e2
T B <tb@Ts-MacBook-Air.local>
2025-02-11 21:02:42 -0500

    closing branch

--- anizone_BE/src/main/java/passion/troy/anizone/entities/StreamingPlatform.java
+++ anizone_BE/src/main/java/passion/troy/anizone/entities/StreamingPlatform.java
@@ -6,20 +6,20 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
-@Entity
-@Table(name= "streaming_platform")
+//@Entity
+//@Table(name= "streaming_platform")
 public class StreamingPlatform {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long platformId;
-    @Column(name = "sp_name")
-    private String name;
-
-    private Map<String, Set<User>> usersByPlatform;
-
-    public StreamingPlatform(){
-
-    }
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long platformId;
+//    @Column(name = "sp_name")
+//    private String name;
+//
+//    private Map<String, Set<User>> usersByPlatform;
+//
+//    public StreamingPlatform(){
+//
+//    }
 
 }
